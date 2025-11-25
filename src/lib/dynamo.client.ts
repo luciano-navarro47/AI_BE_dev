7822889f631a1561fc6d168b556533010b3d78c5
Coderman47 <askinglucho@gmail.com>
2025-11-25 13:01:03 -0300

    fix: validate obj to send to Dynamodbclient

--- src/lib/dynamo.client.ts
+++ src/lib/dynamo.client.ts
@@ -1,8 +1,12 @@
 import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import { DynamoDBDocumentClient } from "@aws-sdk/lib-dynamodb";
 
-const client = new DynamoDBClient({
-    region: process.env.AWS_REGION || "us-east-1"
-});
+const isLocal = process.env.IS_LOCAL === "true";
+
+const client = new DynamoDBClient(
+    isLocal
+        ? { region: process.env.AWS_REGION || "us-east-1", endpoint: "http://localhost:8000" }
+        : { region: process.env.AWS_REGION || "us-east-1" }
+);
 
 export const ddbDocClient = DynamoDBDocumentClient.from(client);
