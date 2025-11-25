e751c57161da5dd2f5d60e8cc557c942cf4892dd
Coderman47 <askinglucho@gmail.com>
2025-11-25 11:47:42 -0300

    tests: create test to validate role

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -16,17 +16,18 @@ interface ServiceResult {
 export const loginService = async (email: string, password: string): Promise<ServiceResult> => {
     try {
         const user = await getUserByEmail(email);
+        console.log("USER: ", user)
         if (!user) {
-            return { ok: false, status: 401, error: "User not found" };
+            return { ok: false, status: 401, error: "Invalid credentials" };
         }
 
         const match = await bcrypt.compare(password, user.passwordHash || "");
         if (!match) {
-            return { ok: false, status: 401, error: "Invalid password" };
+            return { ok: false, status: 401, error: "Invalid credentials" };
         }
 
         if (!rolesMap.has(user.roleId)) {
-            return { ok: false, status: 401, error: "Forbidden: role not allowed" };
+            return { ok: false, status: 403, error: "Forbidden: role not allowed" };
         }
 
         const payload = { userId: user.id, roleId: user.roleId, email: user.email };
