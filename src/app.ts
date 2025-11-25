d3dda94a4f4b43d612676f563326fc558a76ae4b
Coderman47 <askinglucho@gmail.com>
2025-11-24 21:17:43 -0300

    chore: create handler serverless, app with routes and auth middleware to verify jwt

---
+++ src/app.ts
@@ -0,0 +1,13 @@
+import express from "express";
+import dotenv from "dotenv";
+dotenv.config();
+import authRouter from "./routes/auth";
+import meRouter from "./routes/me";
+
+const app = express();
+app.use(express.json());
+
+app.use("/api/v1", authRouter);
+app.use("/api/v1", meRouter);
+
+export default app;
---
+++ src/routes/auth.ts
@@ -0,0 +1,6 @@
+import { Router } from "express";
+import { loginController } from "../controllers/auth.controller";
+const router = Router();
+router.post("/login", loginController);
+router.post("/logout", (req, res) => res.status(204).send());
+export default router;
