e9f4b6a9f4680a1c1b940f7f76ad22fce0baf8d5
Coderman47 <askinglucho@gmail.com>
2025-11-24 21:17:43 -0300

    chore: create handler serverless, app with routes and auth middleware to verify jwt

---
+++ src/middleware/auth.middleware.ts
@@ -0,0 +1,17 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+
+const JWT_SECRET = process.env.JWT_SECRET!;
+
+export function authMiddleware(req: Request, res: Response, next: NextFunction) {
+  const auth = req.headers.authorization;
+  if (!auth) return res.status(401).json({ error: "No token" });
+  const token = auth.split(" ")[1];
+  try {
+    const payload = jwt.verify(token, JWT_SECRET);
+    (req as any).user = payload;
+    next();
+  } catch (err) {
+    return res.status(401).json({ error: "Invalid token" });
+  }
+}
