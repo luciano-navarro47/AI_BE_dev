352a3309cf787eb3c4bf4bf383ab62bf5ed66b1c
Coderman47 <askinglucho@gmail.com>
2025-11-24 22:21:03 -0300

    chore: create helper to JWT to type correctly and implement it in auth service

---
+++ src/lib/jwt.ts
@@ -0,0 +1,11 @@
+import jwt from "jsonwebtoken";
+
+export const signJwt = (
+    payload: object,
+    secret: string,
+    expiresIn: string | number
+): string => {
+    return jwt.sign(payload, secret as jwt.Secret, {
+        expiresIn,
+    } as jwt.SignOptions);
+};
