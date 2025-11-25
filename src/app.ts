cdfcc9cd6c9afaae80fabd74a4008bee4eb5abba
Coderman47 <askinglucho@gmail.com>
2025-11-24 21:42:05 -0300

    chore: create serverless template, env example file and update scripts in package.json

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,20 @@ import express from "express";
 import dotenv from "dotenv";
 dotenv.config();
 import authRouter from "./routes/auth";
-import meRouter from "./routes/me";
+// import meRouter from "./routes/me";
 
 const app = express();
 app.use(express.json());
 
-app.use("/api/v1", authRouter);
-app.use("/api/v1", meRouter);
+app.get("/", (req, res) => {
+    return res.json({ message: "API running" });
+})
+
+app.post("/login", (req, res) => {
+    return res.json({ message: "Login placeholder" });
+})
+
+// app.use("/api/v1", authRouter);
+// app.use("/api/v1", meRouter);
 
 export default app;
