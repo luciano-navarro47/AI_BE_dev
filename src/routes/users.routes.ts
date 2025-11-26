84b1f09de6d754321fca279fcbee1fe0002f2475
Coderman47 <askinglucho@gmail.com>
2025-11-26 14:58:19 -0300

    fix: use admin as a role for users routes

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,14 +1,18 @@
 import { Router } from "express";
 import { authMiddleware } from "../middlewares/auth.middleware";
 import { roleMiddleware } from "../middlewares/role.middleware";
-import { createUserController, getAllUsersController, getUserByIdController } from "../controllers/users.controller";
+import {
+  createUserController,
+  getAllUsersController,
+  getUserByIdController,
+} from "../controllers/users.controller";
 
 const router = Router();
 
-const ADMIN_AUTH = [authMiddleware, roleMiddleware(["personal"])];
+const ADMIN_AUTH = [authMiddleware, roleMiddleware(["admin"])];
 
 // Admin only
-router.post("/users", createUserController);
+router.post("/users", ADMIN_AUTH, createUserController);
 router.get("/users", ADMIN_AUTH, getAllUsersController);
 router.get("/users/:userId", ADMIN_AUTH, getUserByIdController);
 
